c39314048222fe3f4b302b5ff3e071f970f32794
Cory LaNou <cory@joinlocal.com>
2014-04-27 10:31:56 -0600

    some clean up and json helpers

--- src/julia-allyce/yup-rest/endpoints/users/get.go
+++ src/julia-allyce/yup-rest/endpoints/users/get.go
@@ -1,11 +1,9 @@
 package users
 
 import (
-	"encoding/json"
+	"julia-allyce/yup-rest/common/json_helpers"
 	"julia-allyce/yup-rest/models"
-	"log"
 	"net/http"
-	"strconv"
 )
 
 func Get(w http.ResponseWriter, r *http.Request) {
@@ -16,14 +14,5 @@ func Get(w http.ResponseWriter, r *http.Request) {
 		Active: 1,
 	}
 
-	data, err := json.Marshal(user)
-
-	if err != nil {
-		log.Printf("Error marshalling json: %s", err)
-		return
-	}
-
-	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(data)
+	json_helpers.Write(w, user)
 }
